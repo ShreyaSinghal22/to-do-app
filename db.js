b8a30c78ac41184952962ee0fde457e24a3f5baa
ShreyaSinghal22 <shreyasinghal5257@gmail.com>
2025-07-12 00:27:17 +0530

    added the logic of getting the todos back

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 //lets create a particular schema for our application 
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://shreyasinghal5257:shreyasinghal2005@cluster21.i6yit.mongodb.net/");
+mongoose.connect("mongodb+srv://shreyasinghal5257:shreyasinghal2005@cluster21.i6yit.mongodb.net/new-year");
 const myschema = mongoose.schema({
     title: string,
     description: string,
     completed: boolean,
 }) 
 
-const todo = mongoose.model('todos', todoschema);
+const todo = mongoose.model('new-year', todoschema);
 
 module.exports = ({
     todo
--- index.js
+++ index.js
@@ -1,26 +1,45 @@
 const express = require("express");
 const { createtodo } = require("./types");
+const { todo } = require("./db");
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/todo", function(req,res) {
+app.post("/todo", async function (req, res) {
     const createpayload = req.body;
     const parsepayload = createtodo.safeParse(createpayload);
     if (!parsepayload.success) {
         res.status(411).json({
             msg: "you sent the wrong inputs",
-        })
+        });
         return;
     }
+        //put the todos in the database (can only do CRUD)
+    await todo.create({
+        title: createpayload.title,
+        description: createpayload.description,
+        completed: false
+    })
+
+    res.json({
+        msg: "todo created"
+    })
+
 })
 
-app.get("/todos", function(req,res) {
 
+
+app.get("/todos", async function(req,res) {
+    const todos = await todo.find({});//db can be anywhere, might be faraway 
+   //could be a promise if logged
+
+    res.json({
+        todos
+    })
 })
 
-app.put("/completed", function(req,res) {
+app.put("/completed",async function(req,res) {
     const updatepayload = req.body;
     const parsepayload = updatetodo.safeParse(updatepayload);
     if(!updatepayload.success) {
@@ -29,4 +48,12 @@ app.put("/completed", function(req,res) {
         })
         return;
     }
+    await todo.update({
+        _id: req.body.id,
+    },{
+        completed: true
+    })
+    res.json({
+        msg: "todo marked as completed"
+    })
 })
